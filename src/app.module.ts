9e939ba21d54ebadd44b9ecfda03e6fb15718659
Hugo <nandofes13@gmail.com>
2026-02-19 11:14:06 -0300

    configurar TypeORM y conexion a PostgreSQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
 })
-export class AppModule {}
+
