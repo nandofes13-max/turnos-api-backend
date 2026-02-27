d7a066bbcbfabe4ee914b080b27e0de8e6937c55
nandofes13-max <nandofes13@gmail.com>
2026-02-27 10:12:47 -0300

    Rename BaseEntity to BaseEntityAuditable and add fields

--- src/entities/base.entity.ts
+++ src/entities/base.entity.ts
@@ -5,20 +5,29 @@ import {
   Column,
 } from 'typeorm';
 
-export abstract class BaseEntity {
+export abstract class BaseEntityAuditable {
 
   @PrimaryGeneratedColumn()
   id: number;
 
+  // ===== ALTA =====
   @CreateDateColumn({ type: 'timestamp' })
   fecha_alta: Date;
 
+  @Column({ length: 50, nullable: true })
+  usuario_alta: string;
+
+  // ===== MODIFICACIÓN =====
+  @UpdateDateColumn({ type: 'timestamp' })
+  fecha_modificacion: Date;
+
+  @Column({ length: 50, nullable: true })
+  usuario_modificacion: string;
+
+  // ===== BAJA (Soft Delete) =====
   @Column({ type: 'timestamp', nullable: true })
   fecha_baja: Date;
 
   @Column({ length: 50, nullable: true })
-  usuario_creacion: string;
-
-  @UpdateDateColumn({ type: 'timestamp' })
-  ultima_modificacion: Date;
+  usuario_baja: string;
 }
