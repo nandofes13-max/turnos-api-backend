cca96ea81989e00fb819f260856287ddb141b65e
nandofes13-max <nandofes13@gmail.com>
2026-03-02 10:46:12 -0300

    Refactor Filial entity structure
    Refactor Filial entity to extend BaseEntityAuditable and remove id column.

--- src/entities/filial.entity.ts
+++ src/entities/filial.entity.ts
@@ -1,10 +1,9 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+// src/entities/filial.entity.ts
+import { Entity, Column } from 'typeorm';
+import { BaseEntityAuditable } from './base.entity';
 
 @Entity()
-export class Filial {
-  @PrimaryGeneratedColumn()
-  id: number;
-
+export class Filial extends BaseEntityAuditable {
   @Column()
   codigo: string;
 
