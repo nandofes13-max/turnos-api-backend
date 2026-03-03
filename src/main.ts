21c15b8f0980093bfdf2282935db42f11094f6f1
nandofes13-max <nandofes13@gmail.com>
2026-03-02 21:15:39 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,17 @@ import { AppModule } from './app.module';
 import { config } from 'dotenv';
 config();
 
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 🔓 Habilitar CORS para permitir peticiones desde el frontend
+  app.enableCors({
+    origin: 'https://turnos-pwa-frontend.onrender.com', // URL de tu frontend en Render
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   const port = process.env.PORT || 3000;
   await app.listen(port);
-
 }
 bootstrap();
