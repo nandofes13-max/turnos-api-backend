00faacb049391c1b45c40b4e9f1bd3c74da6a316
nandofes13-max <nandofes13@gmail.com>
2026-03-10 17:58:17 -0300

    Create usuarios.module.ts

---
+++ src/usuarios/usuarios.module.ts
@@ -0,0 +1,13 @@
+// src/usuarios/usuarios.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Usuario } from './entities/usuario.entity';
+import { UsuariosService } from './usuarios.service';
+import { UsuariosController } from './usuarios.controller';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Usuario])],
+  providers: [UsuariosService],
+  controllers: [UsuariosController],
+})
+export class UsuariosModule {}
