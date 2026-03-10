5317b7308e7763a096f3256d2e6d5e2daedffced
nandofes13-max <nandofes13@gmail.com>
2026-03-10 17:27:07 -0300

    Create update-usuario.dto.ts

---
+++ src/usuarios/dto/update-usuario.dto.ts
@@ -0,0 +1,5 @@
+// src/usuarios/dto/update-usuario.dto.ts
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateUsuarioDto } from './create-usuario.dto';
+
+export class UpdateUsuarioDto extends PartialType(CreateUsuarioDto) {}
