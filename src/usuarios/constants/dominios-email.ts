aecaa290cd76245ffc3fbd9cfbe713112146ea8a
nandofes13-max <nandofes13@gmail.com>
2026-03-10 19:28:44 -0300

    Create dominios-email.ts

---
+++ src/usuarios/constants/dominios-email.ts
@@ -0,0 +1,42 @@
+// src/usuarios/constants/dominios-email.ts
+export const DOMINIOS_VALIDOS = [
+  // Globales
+  'gmail.com',
+  'hotmail.com',
+  'outlook.com',
+  'yahoo.com',
+  'icloud.com',
+  'aol.com',
+  'protonmail.com',
+  'mail.com',
+  'gmx.com',
+  'yandex.com',
+
+  // Live / Microsoft
+  'live.com',
+  'live.com.ar',
+  'live.com.mx',
+  'live.com.co',
+  'hotmail.com.ar',
+  'hotmail.com.mx',
+  'hotmail.com.co',
+  'outlook.com.ar',
+  'outlook.com.mx',
+  'outlook.com.co',
+
+  // Argentina
+  'fibertel.com.ar',
+  'speedy.com.ar',
+  'arnet.com.ar',
+  'ciudad.com.ar',
+  'sinectis.com.ar',
+  'uolsinectis.com.ar',
+
+  // Gobierno / Educación
+  'gob.ar',
+  'edu.ar',
+  'uba.ar',
+  'unc.edu.ar',
+  'unlp.edu.ar',
+  'utn.edu.ar'
+];
