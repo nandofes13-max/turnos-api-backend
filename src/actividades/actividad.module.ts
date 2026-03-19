15337eb7e6c86353bd921c6dd663ed17cfb908fb
nandofes13-max <nandofes13@gmail.com>
2026-03-19 20:10:01 -0300

    Update actividad.module.ts

--- src/actividades/actividad.module.ts
+++ src/actividades/actividad.module.ts
@@ -4,9 +4,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Actividad } from './entities/actividad.entity';
 import { ActividadService } from './actividad.service';
 import { ActividadController } from './actividad.controller';
+import { Negocio } from '../negocios/entities/negocio.entity'; // 👈 IMPORTAR
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Actividad])],
+  imports: [
+    TypeOrmModule.forFeature([Actividad, Negocio]), // 👈 AGREGAR Negocio
+  ],
   providers: [ActividadService],
   controllers: [ActividadController],
 })
