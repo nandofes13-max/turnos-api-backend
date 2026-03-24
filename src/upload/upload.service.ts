87d47a7835c45cdb9912483f266b1c864d287cb6
nandofes13-max <nandofes13@gmail.com>
2026-03-24 20:19:33 -0300

    Update upload.service.ts

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -4,11 +4,14 @@ import { v2 as cloudinary } from 'cloudinary';
 @Injectable()
 export class UploadService {
   constructor() {
-    cloudinary.config({
-      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-      api_key: process.env.CLOUDINARY_API_KEY,
-      api_secret: process.env.CLOUDINARY_API_SECRET,
-    });
+    // Usar CLOUDINARY_URL si está disponible
+    if (process.env.CLOUDINARY_URL) {
+      cloudinary.config({
+        cloud_name: process.env.CLOUDINARY_URL.split('@')[1].split('.')[0],
+        api_key: process.env.CLOUDINARY_URL.split('://')[1].split(':')[0],
+        api_secret: process.env.CLOUDINARY_URL.split(':')[2].split('@')[0],
+      });
+    }
   }
 
   async uploadImage(base64Image: string): Promise<string> {
