8033ea9ffb8a620b336cd0ee281df9be79b84b41
nandofes13-max <nandofes13@gmail.com>
2026-03-26 18:55:45 -0300

    Update update-centro.dto.ts

--- src/centro/dto/update-centro.dto.ts
+++ src/centro/dto/update-centro.dto.ts
@@ -1,4 +1,15 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCentroDto } from './create-centro.dto';
+import { IsOptional, IsString, IsDate } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateCentroDto extends PartialType(CreateCentroDto) {}
+export class UpdateCentroDto extends PartialType(CreateCentroDto) {
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  fecha_baja?: Date | null;
+
+  @IsOptional()
+  @IsString()
+  usuario_baja?: string | null;
+}
