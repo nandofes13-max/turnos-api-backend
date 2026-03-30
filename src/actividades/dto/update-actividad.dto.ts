b16818226aa5caf58949803fdba87cfea3049212
nandofes13-max <nandofes13@gmail.com>
2026-03-30 19:57:41 -0300

    Update update-actividad.dto.ts

--- src/actividades/dto/update-actividad.dto.ts
+++ src/actividades/dto/update-actividad.dto.ts
@@ -1,16 +1,4 @@
-// src/actividades/dto/update-actividad.dto.ts
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateActividadDto } from './create-actividad.dto';
-import { IsOptional, IsDate, IsString } from 'class-validator';
-import { Type } from 'class-transformer';
 
-export class UpdateActividadDto extends PartialType(CreateActividadDto) {
-  @IsOptional()
-  @IsDate()
-  @Type(() => Date)
-  fecha_baja?: Date | null;
-
-  @IsOptional()
-  @IsString()
-  usuario_baja?: string | null;
-}
+export class UpdateActividadDto extends PartialType(CreateActividadDto) {}
