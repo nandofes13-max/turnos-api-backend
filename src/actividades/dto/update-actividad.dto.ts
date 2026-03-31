01011465463f4f7bd068d4617d8cb1ea9f9d07b1
nandofes13-max <nandofes13@gmail.com>
2026-03-30 21:38:54 -0300

    Add optional fields to UpdateActividadDto

--- src/actividades/dto/update-actividad.dto.ts
+++ src/actividades/dto/update-actividad.dto.ts
@@ -1,4 +1,16 @@
+// src/actividades/dto/update-actividad.dto.ts
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateActividadDto } from './create-actividad.dto';
+import { IsOptional, IsDate, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateActividadDto extends PartialType(CreateActividadDto) {}
+export class UpdateActividadDto extends PartialType(CreateActividadDto) {
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  fecha_baja?: Date | null;
+
+  @IsOptional()
+  @IsString()
+  usuario_baja?: string | null;
+}
