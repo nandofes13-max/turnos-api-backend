fbb92ced2794530f4fb6517a106c86008595a680
nandofes13-max <nandofes13@gmail.com>
2026-03-31 15:59:57 -0300

    Add virtual property to CreateActividadDto

--- src/actividades/dto/create-actividad.dto.ts
+++ src/actividades/dto/create-actividad.dto.ts
@@ -1,9 +1,13 @@
 // src/actividades/dto/create-actividad.dto.ts
-import { IsString, IsNotEmpty, MaxLength } from 'class-validator';
+import { IsString, IsNotEmpty, MaxLength, IsBoolean, IsOptional } from 'class-validator';
 
 export class CreateActividadDto {
   @IsString({ message: 'El nombre debe ser texto' })
   @IsNotEmpty({ message: 'El nombre es obligatorio' })
   @MaxLength(100, { message: 'El nombre no puede tener más de 100 caracteres' })
   nombre: string;
+
+  @IsBoolean({ message: 'Virtual debe ser verdadero o falso' })
+  @IsOptional()
+  virtual?: boolean;
 }
