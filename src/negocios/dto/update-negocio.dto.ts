f4727993313130ce7fb772011f7db08a7b72cf5a
nandofes13-max <nandofes13@gmail.com>
2026-04-03 09:27:29 -0300

    Update update-negocio.dto.ts

--- src/negocios/dto/update-negocio.dto.ts
+++ src/negocios/dto/update-negocio.dto.ts
@@ -1,5 +1,15 @@
-// src/negocios/dto/update-negocio.dto.ts
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateNegocioDto } from './create-negocio.dto';
+import { IsOptional, IsDate, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
 
-export class UpdateNegocioDto extends PartialType(CreateNegocioDto) {}
+export class UpdateNegocioDto extends PartialType(CreateNegocioDto) {
+  @IsOptional()
+  @IsDate()
+  @Type(() => Date)
+  fecha_baja?: Date | null;
+
+  @IsOptional()
+  @IsString()
+  usuario_baja?: string | null;
+}
