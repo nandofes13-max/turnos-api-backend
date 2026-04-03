e05bd27363066cfd7090cfdb8f545f7e6631064e
nandofes13-max <nandofes13@gmail.com>
2026-04-03 10:25:40 -0300

    Add es_virtual property to UpdateCentroDto

--- src/centro/dto/update-centro.dto.ts
+++ src/centro/dto/update-centro.dto.ts
@@ -1,9 +1,13 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateCentroDto } from './create-centro.dto';
-import { IsOptional, IsString, IsDate } from 'class-validator';
+import { IsOptional, IsString, IsDate, IsBoolean } from 'class-validator';
 import { Type } from 'class-transformer';
 
 export class UpdateCentroDto extends PartialType(CreateCentroDto) {
+  @IsOptional()
+  @IsBoolean()
+  es_virtual?: boolean;
+
   @IsOptional()
   @IsDate()
   @Type(() => Date)
