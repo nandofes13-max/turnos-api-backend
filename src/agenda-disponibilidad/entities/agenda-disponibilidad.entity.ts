5f9ad51436d895041c6ec646f8c4aa77350e3ab2
nandofes13-max <nandofes13@gmail.com>
2026-04-07 17:33:12 -0300

    Create agenda-disponibilidad.entity.ts

---
+++ src/agenda-disponibilidad/entities/agenda-disponibilidad.entity.ts
@@ -0,0 +1,38 @@
+import { Entity, Column, ManyToOne, JoinColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntityAuditable } from '../../entities/base.entity';
+import { ProfesionalCentro } from '../../profesional-centro/entities/profesional-centro.entity';
+
+@Entity('agenda_disponibilidad')
+export class AgendaDisponibilidad extends BaseEntityAuditable {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'profesional_centro_id' })
+  profesionalCentroId: number;
+
+  @Column({ name: 'dia_semana', type: 'smallint' })
+  diaSemana: number; // 0=Domingo, 1=Lunes, ..., 6=Sábado
+
+  @Column({ name: 'hora_desde', type: 'time' })
+  horaDesde: string;
+
+  @Column({ name: 'hora_hasta', type: 'time' })
+  horaHasta: string;
+
+  @Column({ name: 'duracion_turno', type: 'int' })
+  duracionTurno: number; // minutos
+
+  @Column({ name: 'buffer_minutos', type: 'int', default: 0 })
+  bufferMinutos: number;
+
+  @Column({ name: 'fecha_desde', type: 'date' })
+  fechaDesde: Date;
+
+  @Column({ name: 'fecha_hasta', type: 'date', nullable: true })
+  fechaHasta: Date | null;
+
+  // Relaciones
+  @ManyToOne(() => ProfesionalCentro)
+  @JoinColumn({ name: 'profesional_centro_id' })
+  profesionalCentro: ProfesionalCentro;
+}
