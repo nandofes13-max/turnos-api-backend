d9a1e1b886fe74724b48e76b910f03e013ed2405
nandofes13-max <nandofes13@gmail.com>
2026-04-16 11:26:17 -0300

    Delete src/agenda-excepciones directory

--- src/agenda-excepciones/dto/update-agenda-excepcion.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAgendaExcepcionDto } from './create-agenda-excepcion.dto';
-
-export class UpdateAgendaExcepcionDto extends PartialType(CreateAgendaExcepcionDto) {}
