c4bdf14080f4338a557eac82ebac1dda420bc565
nandofes13-max <nandofes13@gmail.com>
2026-04-16 20:16:09 -0300

    Add ExcepcionRecurrente and ExcepcionFecha to module

--- src/agenda-disponibilidad/agenda-disponibilidad.module.ts
+++ src/agenda-disponibilidad/agenda-disponibilidad.module.ts
@@ -1,15 +1,20 @@
+// src/agenda-disponibilidad/agenda-disponibilidad.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AgendaDisponibilidad } from './entities/agenda-disponibilidad.entity';
 import { AgendaDisponibilidadService } from './agenda-disponibilidad.service';
 import { AgendaDisponibilidadController } from './agenda-disponibilidad.controller';
 import { ProfesionalCentro } from '../profesional-centro/entities/profesional-centro.entity';
+import { ExcepcionRecurrente } from '../excepciones-recurrentes/entities/excepcion-recurrente.entity';
+import { ExcepcionFecha } from '../excepciones-fechas/entities/excepcion-fecha.entity';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([
       AgendaDisponibilidad,
       ProfesionalCentro,
+      ExcepcionRecurrente,  // 👈 AGREGAR
+      ExcepcionFecha,       // 👈 AGREGAR
     ])
   ],
   controllers: [AgendaDisponibilidadController],
