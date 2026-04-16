4af940508d57fa1d6e8ffe2d07774705cb9130c2
nandofes13-max <nandofes13@gmail.com>
2026-04-16 15:14:04 -0300

    Simplify UpdateExcepcionRecurrenteDto class
    Removed optional id property and its validation from UpdateExcepcionRecurrenteDto.

--- src/excepciones-recurrentes/dto/update-excepcion-recurrente.dto.ts
+++ src/excepciones-recurrentes/dto/update-excepcion-recurrente.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateExcepcionRecurrenteDto } from './create-excepcion-recurrente.dto';
-import { IsOptional, IsNumber } from 'class-validator';
 
-export class UpdateExcepcionRecurrenteDto extends PartialType(CreateExcepcionRecurrenteDto) {
-  @IsOptional()
-  @IsNumber({}, { message: 'id debe ser un número' })
-  id?: number;
-}
+export class UpdateExcepcionRecurrenteDto extends PartialType(CreateExcepcionRecurrenteDto) {}
