3f36d3b5477b7a2f59e663bf2802bebe13d138cc
nandofes13-max <nandofes13@gmail.com>
2026-04-16 14:18:23 -0300

    Update update-excepcion-recurrente.dto.ts

--- src/excepciones-recurrentes/dto/update-excepcion-recurrente.dto.ts
+++ src/excepciones-recurrentes/dto/update-excepcion-recurrente.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateExcepcionRecurrenteDto } from './create-excepcion-recurrente.dto';
+import { IsOptional, IsNumber } from 'class-validator';
 
-export class UpdateExcepcionRecurrenteDto extends PartialType(CreateExcepcionRecurrenteDto) {}
+export class UpdateExcepcionRecurrenteDto extends PartialType(CreateExcepcionRecurrenteDto) {
+  @IsOptional()
+  @IsNumber({}, { message: 'id debe ser un número' })
+  id?: number;
+}
