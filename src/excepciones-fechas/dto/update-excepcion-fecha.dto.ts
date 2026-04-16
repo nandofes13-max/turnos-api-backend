8e2c4182b7d37e7a022fb2c2aabd3e44adfc2aba
nandofes13-max <nandofes13@gmail.com>
2026-04-16 14:53:42 -0300

    Simplify UpdateExcepcionFechaDto by removing id
    Removed optional id property and its validations from UpdateExcepcionFechaDto.

--- src/excepciones-fechas/dto/update-excepcion-fecha.dto.ts
+++ src/excepciones-fechas/dto/update-excepcion-fecha.dto.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateExcepcionFechaDto } from './create-excepcion-fecha.dto';
-import { IsOptional, IsNumber } from 'class-validator';
 
-export class UpdateExcepcionFechaDto extends PartialType(CreateExcepcionFechaDto) {
-  @IsOptional()
-  @IsNumber({}, { message: 'id debe ser un número' })
-  id?: number;
-}
+export class UpdateExcepcionFechaDto extends PartialType(CreateExcepcionFechaDto) {}
