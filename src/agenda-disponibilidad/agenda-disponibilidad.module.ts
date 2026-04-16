7932dcb55ecb4bc9526ddfa12ba8380aa94681c6
nandofes13-max <nandofes13@gmail.com>
2026-04-15 22:00:58 -0300

    Add AgendaExcepcion to TypeOrmModule imports

--- src/agenda-disponibilidad/agenda-disponibilidad.module.ts
+++ src/agenda-disponibilidad/agenda-disponibilidad.module.ts
@@ -4,10 +4,15 @@ import { AgendaDisponibilidad } from './entities/agenda-disponibilidad.entity';
 import { AgendaDisponibilidadService } from './agenda-disponibilidad.service';
 import { AgendaDisponibilidadController } from './agenda-disponibilidad.controller';
 import { ProfesionalCentro } from '../profesional-centro/entities/profesional-centro.entity';
+import { AgendaExcepcion } from '../agenda-excepciones/entities/agenda-excepcion.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([AgendaDisponibilidad, ProfesionalCentro])
+    TypeOrmModule.forFeature([
+      AgendaDisponibilidad,
+      ProfesionalCentro,
+      AgendaExcepcion,  // 👈 AGREGADO
+    ])
   ],
   controllers: [AgendaDisponibilidadController],
   providers: [AgendaDisponibilidadService],
