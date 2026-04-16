03baa3d311a811c8d1293cf2585802f4e0306794
nandofes13-max <nandofes13@gmail.com>
2026-04-16 14:16:21 -0300

    Add optional id field to UpdateExcepcionFechaDto

--- src/excepciones-fechas/dto/update-excepcion-fecha.dto.ts
+++ src/excepciones-fechas/dto/update-excepcion-fecha.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateExcepcionFechaDto } from './create-excepcion-fecha.dto';
+import { IsOptional, IsNumber } from 'class-validator';
 
-export class UpdateExcepcionFechaDto extends PartialType(CreateExcepcionFechaDto) {}
+export class UpdateExcepcionFechaDto extends PartialType(CreateExcepcionFechaDto) {
+  @IsOptional()
+  @IsNumber({}, { message: 'id debe ser un número' })
+  id?: number;
+}
