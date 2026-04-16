f1e2db83585ce282a1c5329575c688ee5a60e598
nandofes13-max <nandofes13@gmail.com>
2026-04-16 11:27:52 -0300

    Create ExcepcionFecha entity with date and time fields

---
+++ src/excepciones-fechas/entities/excepcion-fecha.entity.ts
@@ -0,0 +1,35 @@
+import { Entity, Column, ManyToOne, JoinColumn, PrimaryGeneratedColumn } from 'typeorm';
+import { BaseEntityAuditable } from '../../entities/base.entity';
+import { AgendaDisponibilidad } from '../../agenda-disponibilidad/entities/agenda-disponibilidad.entity';
+
+@Entity('excepciones_fechas')
+export class ExcepcionFecha extends BaseEntityAuditable {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'agenda_disponibilidad_id' })
+  agendaDisponibilidadId: number;
+
+  @Column({ name: 'fecha_desde', type: 'date' })
+  fechaDesde: Date;
+
+  @Column({ name: 'fecha_hasta', type: 'date', nullable: true })
+  fechaHasta: Date | null;
+
+  @Column({ name: 'hora_desde', type: 'time', nullable: true })
+  horaDesde: string | null;
+
+  @Column({ name: 'hora_hasta', type: 'time', nullable: true })
+  horaHasta: string | null;
+
+  @Column({ type: 'varchar', length: 20 })
+  tipo: string; // 'deshabilitado' | 'bloqueado'
+
+  @Column({ type: 'text', nullable: true })
+  motivo: string | null;
+
+  // Relaciones
+  @ManyToOne(() => AgendaDisponibilidad)
+  @JoinColumn({ name: 'agenda_disponibilidad_id' })
+  agendaDisponibilidad: AgendaDisponibilidad;
+}
