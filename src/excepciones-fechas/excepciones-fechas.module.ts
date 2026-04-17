cd1a8baa5b40fc30d000caa6797f638fbf35d1fe
nandofes13-max <nandofes13@gmail.com>
2026-04-17 15:05:57 -0300

    Remove AgendaDisponibilidad import and add ProfesionalCentroModule

--- src/excepciones-fechas/excepciones-fechas.module.ts
+++ src/excepciones-fechas/excepciones-fechas.module.ts
@@ -1,13 +1,15 @@
+// src/excepciones-fechas/excepciones-fechas.module.ts
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ExcepcionFecha } from './entities/excepcion-fecha.entity';
 import { ExcepcionesFechasService } from './excepciones-fechas.service';
 import { ExcepcionesFechasController } from './excepciones-fechas.controller';
-import { AgendaDisponibilidad } from '../agenda-disponibilidad/entities/agenda-disponibilidad.entity';
+import { ProfesionalCentroModule } from '../profesional-centro/profesional-centro.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ExcepcionFecha, AgendaDisponibilidad])
+    TypeOrmModule.forFeature([ExcepcionFecha]),
+    ProfesionalCentroModule, // 👈 Importar el módulo completo
   ],
   controllers: [ExcepcionesFechasController],
   providers: [ExcepcionesFechasService],
