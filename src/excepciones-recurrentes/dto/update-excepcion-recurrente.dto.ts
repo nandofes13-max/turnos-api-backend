1c6a06ab196d9557c1d4a9fb6ac5ac0a419dd33e
nandofes13-max <nandofes13@gmail.com>
2026-04-20 15:53:47 -0300

    Delete src/excepciones-recurrentes directory

--- src/excepciones-recurrentes/dto/update-excepcion-recurrente.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateExcepcionRecurrenteDto } from './create-excepcion-recurrente.dto';
-
-export class UpdateExcepcionRecurrenteDto extends PartialType(CreateExcepcionRecurrenteDto) {}
