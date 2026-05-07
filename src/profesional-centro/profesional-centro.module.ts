c99bc48cf77f8a59053652bd99cbd0a9fa12bb1c
nandofes13-max <nandofes13@gmail.com>
2026-05-06 21:33:35 -0300

    Add additional entities to ProfesionalCentroModule

--- src/profesional-centro/profesional-centro.module.ts
+++ src/profesional-centro/profesional-centro.module.ts
@@ -1,15 +1,26 @@
-// src/profesional-centro/profesional-centro.module.ts
 import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfesionalCentro } from './entities/profesional-centro.entity';
 import { ProfesionalCentroService } from './profesional-centro.service';
 import { ProfesionalCentroController } from './profesional-centro.controller';
+import { Profesional } from '../profesional/entities/profesional.entity';
+import { Especialidad } from '../especialidades/entities/especialidad.entity';
+import { Centro } from '../centro/entities/centro.entity';
+import { ActividadEspecialidad } from '../actividad-especialidad/entities/actividad-especialidad.entity';
+import { NegocioActividad } from '../negocio-actividades/entities/negocio-actividad.entity';
 import { CentroModule } from '../centro/centro.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ProfesionalCentro]),
-    forwardRef(() => CentroModule),  // 👈 AGREGAR si es necesario
+    TypeOrmModule.forFeature([
+      ProfesionalCentro, 
+      Profesional, 
+      Especialidad, 
+      Centro,
+      ActividadEspecialidad,
+      NegocioActividad
+    ]),
+    forwardRef(() => CentroModule),  // 👈 AGREGAR para resolver dependencia circular
   ],
   controllers: [ProfesionalCentroController],
   providers: [ProfesionalCentroService],
