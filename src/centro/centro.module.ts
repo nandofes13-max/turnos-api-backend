fa408d51935b66e11e413a3b4ebc3e1e41715aab
nandofes13-max <nandofes13@gmail.com>
2026-05-06 21:23:44 -0300

    Add ProfesionalCentroModule to CentroModule imports

--- src/centro/centro.module.ts
+++ src/centro/centro.module.ts
@@ -4,11 +4,13 @@ import { Centro } from './entities/centro.entity';
 import { CentroService } from './centro.service';
 import { CentroController } from './centro.controller';
 import { NegociosModule } from '../negocios/negocios.module';
+import { ProfesionalCentroModule } from '../profesional-centro/profesional-centro.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Centro]),
     forwardRef(() => NegociosModule),
+    forwardRef(() => ProfesionalCentroModule),  // 👈 AGREGAR
   ],
   controllers: [CentroController],
   providers: [CentroService],
