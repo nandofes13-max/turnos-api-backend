33aca19bbca4f33060e49935ad1c2ae68da7b05a
nandofes13-max <nandofes13@gmail.com>
2026-05-06 21:22:54 -0300

    Update profesional-centro.module.ts

--- src/profesional-centro/profesional-centro.module.ts
+++ src/profesional-centro/profesional-centro.module.ts
@@ -1,24 +1,15 @@
-import { Module } from '@nestjs/common';
+// src/profesional-centro/profesional-centro.module.ts
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProfesionalCentro } from './entities/profesional-centro.entity';
 import { ProfesionalCentroService } from './profesional-centro.service';
 import { ProfesionalCentroController } from './profesional-centro.controller';
-import { Profesional } from '../profesional/entities/profesional.entity';
-import { Especialidad } from '../especialidades/entities/especialidad.entity';
-import { Centro } from '../centro/entities/centro.entity';
-import { ActividadEspecialidad } from '../actividad-especialidad/entities/actividad-especialidad.entity';
-import { NegocioActividad } from '../negocio-actividades/entities/negocio-actividad.entity';
+import { CentroModule } from '../centro/centro.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([
-      ProfesionalCentro, 
-      Profesional, 
-      Especialidad, 
-      Centro,
-      ActividadEspecialidad,
-      NegocioActividad
-    ])
+    TypeOrmModule.forFeature([ProfesionalCentro]),
+    forwardRef(() => CentroModule),  // 👈 AGREGAR si es necesario
   ],
   controllers: [ProfesionalCentroController],
   providers: [ProfesionalCentroService],
