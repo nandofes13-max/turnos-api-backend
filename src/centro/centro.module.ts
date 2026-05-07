a20b1681c5143795012551d89f21d1dc1f83eadb
nandofes13-max <nandofes13@gmail.com>
2026-05-06 21:14:10 -0300

    Update centro.module.ts

--- src/centro/centro.module.ts
+++ src/centro/centro.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Centro } from './entities/centro.entity';
 import { CentroService } from './centro.service';
 import { CentroController } from './centro.controller';
+import { NegociosModule } from '../negocios/negocios.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Centro])],
+  imports: [
+    TypeOrmModule.forFeature([Centro]),
+    forwardRef(() => NegociosModule),
+  ],
   controllers: [CentroController],
   providers: [CentroService],
   exports: [CentroService],
