969a1786cc6a40f3d4a8ffe42be32ce74dba83b9
nandofes13-max <nandofes13@gmail.com>
2026-05-06 21:14:39 -0300

    Add forwardRef for CentroModule in NegociosModule

--- src/negocios/negocios.module.ts
+++ src/negocios/negocios.module.ts
@@ -1,13 +1,18 @@
 // src/negocios/negocios.module.ts
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Negocio } from './entities/negocio.entity';
 import { NegociosService } from './negocios.service';
 import { NegociosController } from './negocios.controller';
+import { CentroModule } from '../centro/centro.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Negocio])],
+  imports: [
+    TypeOrmModule.forFeature([Negocio]),
+    forwardRef(() => CentroModule),
+  ],
   providers: [NegociosService],
   controllers: [NegociosController],
+  exports: [NegociosService],
 })
 export class NegociosModule {}
