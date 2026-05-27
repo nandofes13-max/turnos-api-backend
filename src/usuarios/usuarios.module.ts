e3305c91b832d207e082acdf4ed298ab47e84b16
nandofes13-max <nandofes13@gmail.com>
2026-05-27 20:27:16 -0300

    Update usuarios.module.ts

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -2,11 +2,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Usuario } from './entities/usuario.entity';
+import { NegocioUsuarioRol } from '../negocios-usuarios-roles/entities/negocio-usuario-rol.entity';
+import { Rol } from '../roles/entities/rol.entity';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario])],
+  imports: [TypeOrmModule.forFeature([Usuario, NegocioUsuarioRol, Rol])],
   providers: [UsuariosService],
   controllers: [UsuariosController],
 })
