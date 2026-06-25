7a83f753dd0aeefdc3096cd3f314fecfffe85fe7
nandofes13-max <nandofes13@gmail.com>
2026-06-25 19:27:40 -0300

    Add NotificationsController to NotificationsModule

--- src/notifications/notifications.module.ts
+++ src/notifications/notifications.module.ts
@@ -1,9 +1,12 @@
+// src/notifications/notifications.module.ts
 import { Module } from '@nestjs/common';
 import { NotificationsService } from './notifications.service';
+import { NotificationsController } from './notifications.controller'; // 👈 NUEVO import
 import { EmailService } from './email.service';
 
 @Module({
   providers: [NotificationsService, EmailService],
+  controllers: [NotificationsController], // 👈 NUEVO: agregar controller
   exports: [NotificationsService, EmailService],
 })
 export class NotificationsModule {}
